b546fe2e959da10c994e5c5a849858a71810767f
Shariq <shariqsd2003@gmail.com>
2024-07-15 23:53:25 +0530

    Final touch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import reviews from './data';
+import Testimonials from './components/Testimonials';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+      <div className='heading-container'>
+        <h1 >Our Testimonials</h1>
+        <div className='underline'></div>
+      </div>
+        <div>
+          <Testimonials reviews={reviews}/>
+        </div>
+      </div>
+    </>
   );
 }
 
